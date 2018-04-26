b4de649a691aa16d7fb994fe786715cc17ba694a
Rajat Chouksey <rajat.chouksey07@gmail.com>
2018-04-26 12:41:23 +0530

    first commit

---
+++ src/main/java/com/camunda/demo/SimpleDemo/CheckWeatherDelegate.java
@@ -0,0 +1,17 @@
+package com.camunda.demo.SimpleDemo;
+
+import java.util.Random;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class CheckWeatherDelegate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		Random rando = new Random();
+		execution.setVariable("name", "RAJAT");
+		execution.setVariable("weatherOk", rando.nextBoolean());
+	}
+
+}
